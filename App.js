52a2abf3ed155cb2c4b87b8eeaaa78f2c4f0d90f
edend85 <117260021+edend85@users.noreply.github.com>
2023-04-27 16:44:42 +0300

    app change again...

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import Register from './src/Screens/Register';
+import Login from './src/Screens/Login';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Register></Register>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+       <Stack.Navigator initialRouteName='Login'>
+        <Stack.Screen name='Login' component={Login} options={{headerShown: false}} />
+        <Stack.Screen name='Register' component={Register} options={{headerShown: false}} />
+        {/* <Stack.Screen name='userScreens' component={UserTab} options={{headerShown: false}} ></Stack.Screen>
+        <Stack.Screen name='adminScreens' component={AdminTab} options={{headerShown: false}} ></Stack.Screen> */}
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
