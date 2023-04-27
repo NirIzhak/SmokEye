ca674d85601aea53899eb660e6d60967559794bd
Nir Izhak <nir676676@gmail.com>
2023-04-27 15:53:47 +0300

    Add Login Page

--- src/Screens/Login.jsx
+++ src/Screens/Login.jsx
@@ -1,10 +1,81 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useRef, useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handlePress = () => {
+    Keyboard.dismiss();
+  };
+
   return (
-    <View>
-      <Text>Login</Text>
-    </View>
-  )
+    <TouchableWithoutFeedback onPress={handlePress}>
+      <View style={{ direction: "rtl" }}>
+        <Text style={{ textAlign: "center", marginTop: 110, fontSize: 40 }}>
+          התחברות
+        </Text>
+        <TextInput
+          onChangeText={(text) => setEmail(text)}
+          placeholder="מייל"
+          style={{
+            borderBottomWidth: 1,
+            width: "60%",
+            marginLeft: "auto",
+            marginRight: "auto",
+            marginTop: 50,
+            textAlign: "center",
+            fontSize: 28,
+          }}
+        />
+        <TextInput
+          onChangeText={(text) => setPassword(text)}
+          placeholder="סיסמא"
+          secureTextEntry={true}
+          style={{
+            borderBottomWidth: 1,
+            width: "60%",
+            marginLeft: "auto",
+            marginRight: "auto",
+            marginTop: 50,
+            textAlign: "center",
+            fontSize: 28,
+          }}
+          onBlur={() => Keyboard.dismiss()}
+        />
+        <TouchableOpacity>
+          <Text
+            onPress={() => {
+                console.log(password, email)
+            }}
+            style={{ textAlign: "center", marginTop: 50 }}
+          >
+            התחברות
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={{marginTop: 100}}>
+          <Text style={{ textAlign: "center", borderWidth: 1 }}>
+            אין לך משתמש עדיין? הרשם עכשיו!
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={{marginTop: 100}}>
+          <Text style={{ textAlign: "center", borderWidth: 1 }}>
+            התחברות מהירה עם גוגל
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={{marginTop: 100}}>
+          <Text style={{ textAlign: "center", borderWidth: 1 }}>
+            דיווח מהיר ללא התחברות
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </TouchableWithoutFeedback>
+  );
 }
