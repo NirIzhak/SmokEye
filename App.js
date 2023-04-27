a7c170ddefac1aed7ea4767c7ab25ab92e703755
edend85 <117260021+edend85@users.noreply.github.com>
2023-04-27 15:56:49 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Register from './src/Components/Register';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>hi!</Text>
+      
       <StatusBar style="auto" />
     </View>
   );
