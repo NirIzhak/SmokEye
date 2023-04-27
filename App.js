a5d96cc54ffd8cff9b5d686a201f9e7346d394e4
Nir Izhak <nir676676@gmail.com>
2023-04-27 16:23:37 +0300

    Add Stacks

--- App.js
+++ App.js
@@ -1,22 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Register from './src/Components/Register';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import Register from './src/Screens/Register';
+import Login from './src/Screens/Login';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>hi!</Text>
-      
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+       <Stack.Navigator initialRouteName='Login'>
+        <Stack.Screen name='Login' component={Login} options={{headerShown: false}} />
+        <Stack.Screen name='Register' component={Register} options={{headerShown: false}} />
+        {/* <Stack.Screen name='userScreens' component={UserTab} options={{headerShown: false}} ></Stack.Screen>
+        <Stack.Screen name='adminScreens' component={AdminTab} options={{headerShown: false}} ></Stack.Screen> */}
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
+
+
+
