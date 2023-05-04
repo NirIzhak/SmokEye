79f8de01dd533270d8571506688f4f8686102294
Nir Izhak <nir676676@gmail.com>
2023-05-04 16:20:59 +0300

    Add Find Address

--- src/Screens/NewReport.jsx
+++ src/Screens/NewReport.jsx
@@ -1,36 +1,64 @@
-import { View, Text, TextInput } from "react-native";
-import React, { useEffect, useState } from "react";
-import * as Location  from "expo-location";
+import { View, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, Keyboard } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import * as Location from "expo-location";
 
 export default function NewReport() {
   const date = new Date();
   const [location, setLocation] = useState({});
+  const [addressInfo, setAddressInfo] = useState({});
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+  const [address, setAddress] = useState("");
+
+  const handlePress = () => {
+    Keyboard.dismiss();
+  };
+
+  const GetAddress = async () => {
+    const url = `https://address-from-to-latitude-longitude.p.rapidapi.com/geolocationapi?lat=${latitude}&lng=${longitude}`;
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "11a0f318a4msh5d32c1a19d99036p144455jsn538c51d6c8f1",
+        "X-RapidAPI-Host": "address-from-to-latitude-longitude.p.rapidapi.com",
+      },
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      console.log(result);
+      setAddressInfo(result);
+      setAddress(result.Results[0].address);
+
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    const getPermissions = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.log("Not Granted");
+        return;
+      }
+
+      let currentLocation = await Location.getCurrentPositionAsync({});
+      setLocation(currentLocation);
+      console.log("Location:");
+      console.log(currentLocation);
+      setLatitude(currentLocation.coords.latitude);
+      setLongitude(currentLocation.coords.longitude);
+    };
+    getPermissions();
+  }, []);
 
 
-  
-
-
-
-
-
-useEffect(()=>{
-const getPermissions = async()=>{
-  let {status} = await Location.requestForegroundPermissionsAsync();
-  if(status !== 'granted'){
-    console.log("Not Granted")
-    return;
-  }
-
-  let currentLocation = await Location.getCurrentPositionAsync({});
-  setLocation(currentLocation);
-  console.log("Location:")
-  console.log(currentLocation)
-};
-getPermissions();
-},[])
-
+ 
 
   return (
+    <TouchableWithoutFeedback onPress={handlePress}>
     <View>
       <Text style={{ textAlign: "center", marginTop: 110, fontSize: 40 }}>
         על מה הדיווח?
@@ -55,18 +83,6 @@ getPermissions();
       <Text style={{ marginTop: 40, textAlign: "center" }}>
         תמונות \ סרטונים
       </Text>
-
-      <Text style={{ marginTop: 40, textAlign: "center" }}>כתובת מדוייקת</Text>
-      <TextInput
-        placeholder="לא חובה"
-        style={{
-          borderBottomWidth: 1,
-          width: "60%",
-          marginLeft: "auto",
-          marginRight: "auto",
-          textAlign: "right",
-        }}
-      />
       <View
         style={{
           display: "flex",
@@ -91,11 +107,34 @@ getPermissions();
         <View>
           <Text style={{ textAlign: "center" }}>שעה</Text>
           <Text style={{ textAlign: "center" }}>
-            {date.getHours().toString().padStart(2, '0') + ":" + date.getMinutes().toString().padStart(2, '0')}
+            {date.getHours().toString().padStart(2, "0") +
+              ":" +
+              date.getMinutes().toString().padStart(2, "0")}
           </Text>
         </View>
       </View>
-      <Text>Press</Text>
+      <View style={{ marginTop: 20 }}>
+        <Text style={{ textAlign: "center" }}>כתובת:</Text>
+        <TextInput
+          placeholder="כתובת מלאה" 
+          defaultValue={address}         
+          style={{
+            borderBottomWidth: 1,
+            width: "30%",
+            marginLeft: "auto",
+            marginRight: "auto",
+            textAlign: "right",
+          }}
+        />
+        <TouchableOpacity
+          onPress={() => {
+             GetAddress();
+          }}
+        >
+          <Text style={{ textAlign: "center", marginTop: 10 }}>מצא כתובת</Text>
+        </TouchableOpacity>
+      </View>
     </View>
+    </TouchableWithoutFeedback>
   );
 }
