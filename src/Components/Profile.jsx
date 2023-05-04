0c70cc8273add5aa14ec7aa0007eb57ce229f2d6
edend85 <117260021+edend85@users.noreply.github.com>
2023-05-04 19:54:38 +0300

    Update Profile.jsx

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -6,7 +6,7 @@ export default function Profile() {
     <View style={{flex:1,justifyContent:'center'}}>
       <Image source={{uri: 'https://www.bswitch.semicom.co.il/wp-content/uploads/2021/08/8011564373075_1.jpg'}} 
       style={{ width: 100,height: 100}}></Image>
-      <Text>here we place the full name </Text>
+      <Text>here we place the full name.... </Text>
       <Text>mail address</Text>
       <Button title='עריכת פרטים'></Button>
     </View>
