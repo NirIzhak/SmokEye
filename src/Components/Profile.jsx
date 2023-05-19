e371d3294ad48eee36558de801b60195dc377159
edend85 <117260021+edend85@users.noreply.github.com>
2023-05-19 15:31:18 +0300

    Update Profile.jsx

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,4 +1,4 @@
-import { View, Text,Image,TouchableOpacity,Button, SafeAreaView } from 'react-native'
+import { View, Text,Image,TouchableOpacity,Button, SafeAreaView,StyleSheet } from 'react-native'
 import React, { useContext } from 'react'
 import { SmokeyeContext } from '../Context/SmokEyeContext'
 import { Avatar } from "@react-native-material/core";
@@ -8,13 +8,31 @@ export default function Profile() {
   const {currentUser} = useContext(SmokeyeContext);
 
   return (
-    <SafeAreaView >
-      <View style={{alignItems: 'center'}}>
-      {currentUser.image ? (<Avatar image={{ uri: `${currentUser.image}` }} size={150} />) : (<Avatar label={currentUser.firstName + " " + currentUser.lastName} autoColor size={150} />)}
-      <Text style={{fontSize: 40}}>{currentUser.firstName} {currentUser.lastName}</Text>
-      <Text style={{fontSize: 30}}>{currentUser.email}</Text>
+    <SafeAreaView style={styles.continer} >
+      <View style={styles.profileView}>
+        <View style={{ marginRight: 20 }}>
+        {currentUser.image ? 
+        (<Avatar image={{ uri: `${currentUser.image}` }} size={100} />) 
+        : 
+        (<Avatar label={currentUser.firstName + " " + currentUser.lastName} autoColor size={150} />)}
+        </View>
+        <View style={styles.continer}>
+        <Text style={{fontSize: 24}}>{currentUser.firstName} {currentUser.lastName}</Text>
+        <Text style={{fontSize: 20}}>{currentUser.email}</Text>
+        </View>
       </View>
       <Button title='עריכת פרטים'></Button>
     </SafeAreaView>
   )
 }
+const styles= StyleSheet.create({
+  continer:{
+    flex:1,
+    margin:15
+  },
+  profileView:{
+    flexDirection: 'row', 
+    alignItems: 'center', 
+    justifyContent: 'flex-end'
+  }
+})
