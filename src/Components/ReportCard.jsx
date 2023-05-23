13e3dd634c18c059b86376e6e72cc7a79e1e0d1b
edend85 <117260021+edend85@users.noreply.github.com>
2023-05-23 18:07:03 +0300

    Update ReportCard.jsx

--- src/Components/ReportCard.jsx
+++ src/Components/ReportCard.jsx
@@ -1,18 +1,64 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native'
+import { View, Text, Image, TouchableOpacity,StyleSheet } from 'react-native'
 import React from 'react'
 
 export default function ReportCard({id, description, media, location, address, date, navigation}) {
+
+  const dateArr = date.split(" ");
+  /*{ uri: media }*/
   return (
     <TouchableOpacity
       onPress={() => {
         navigation.navigate("fullReport", { id, description, media, location, address, date });
       }}
     >
-    <View style={{borderWidth: 0.5, marginVertical: 5, width: '80%', marginRight: 'auto', marginLeft: 'auto', alignItems:'center', borderRadius: 6, height: 350}}>
-      <Text style={{fontSize: 25, margin: 5}}>{description}</Text>
-      <Text style={{fontSize: 15, margin: 5}}>{address}</Text>
-      <Image source={{ uri: media }} style={{ width: '90%', height: '80%', borderRadius: 4 }} />
+    <View style={styles.continer}>
+      <View>
+      <Image source={media ? {uri:media} : {uri:'https://wallpapercave.com/wp/wp6681156.jpg'}} style={styles.img} />
+      </View>
+   <View style={styles.text_continer}>
+   <Text style={styles.text_field}>
+        <Text style={styles.text_bold}>תאריך : </Text>
+        <Text>{dateArr[0]}</Text>
+    </Text>
+    <Text> 
+      <Text style={styles.text_bold}>עסק/אחר : </Text>
+      <Text>עסק </Text>
+    </Text>
+    <Text style={styles.text_field}>
+        <Text style={styles.text_bold}>כתובת : </Text>
+        <Text>{address}</Text>
+        </Text>
+      <Text style={styles.text_field}>
+        <Text style={styles.text_bold}>תיאור : </Text>
+        <Text>{description}</Text>
+        </Text>
+   </View>
     </View>
     </TouchableOpacity>
   )
 }
+const styles = StyleSheet.create({
+  continer:{
+    direction:'rtl',
+    borderColor:'#D9D9D9',
+    borderWidth:2,
+    margin:5,
+    borderRadius:5,
+    flexDirection:'row',
+  },
+  text_bold:{
+    fontWeight:'bold'
+  },
+  text_continer:{
+    gap:2,
+    margin:10
+  },
+  text_field:{
+    fontSize: 15
+  },
+  img:{
+    width:100,
+    height:100,
+    margin:5
+  }
+})
