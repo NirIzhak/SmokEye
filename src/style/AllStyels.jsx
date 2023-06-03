e43a6a6128eb05b382854756001df501d932f1a1
edend85 <117260021+edend85@users.noreply.github.com>
2023-06-03 13:53:58 +0300

    Update AllStyels.jsx

--- src/style/AllStyels.jsx
+++ src/style/AllStyels.jsx
@@ -3,10 +3,11 @@ export const Colors={
   primary:'#F39508',
   black: '#000',
   white: '#fff',
-  borderColor:"#8C8A89"
+  borderColor:"#8C8A89",
+  transparent:"transparent"
 }
 export const fontSizes={
-  XL:26,
+  XL:22,
   L: 20,
   M: 18,
   S: 16
