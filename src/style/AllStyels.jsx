eaefa54710744ec8648385fb7458f136d13faf34
edend85 <117260021+edend85@users.noreply.github.com>
2023-06-03 12:44:43 +0300

    Create AllStyels.jsx

---
+++ src/style/AllStyels.jsx
@@ -0,0 +1,15 @@
+
+export const Colors={
+  primary:'#F39508',
+  black: '#000',
+  white: '#fff',
+  borderColor:"#8C8A89"
+}
+export const fontSizes={
+  XL:26,
+  L: 20,
+  M: 18,
+  S: 16
+}
+
+  
