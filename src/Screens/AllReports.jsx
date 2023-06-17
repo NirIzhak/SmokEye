36f547fec5a6362ab54c9e0550982f7e72b3f522
edend85 <117260021+edend85@users.noreply.github.com>
2023-06-17 19:29:28 +0300

    Update AllReports.jsx

--- src/Screens/AllReports.jsx
+++ src/Screens/AllReports.jsx
@@ -1,20 +1,19 @@
-import { View, Text, FlatList,StyleSheet } from "react-native";
+import { View, Text, FlatList, StyleSheet } from "react-native";
 import React, { useContext } from "react";
 import { SmokeyeContext } from "../Context/SmokEyeContext";
 import ReportCard from "../Components/ReportCard";
 
-export default function MyReports({navigation}) {
+export default function MyReports({ navigation }) {
   const { allReports } = useContext(SmokeyeContext);
-
   return (
     <View style={styles.continer} >
-      <Text style={[styles.continer_title,styles.title]}>
+      <Text style={[styles.continer_title, styles.title]}>
         כל הדיווחים
       </Text>
       <FlatList
         data={allReports}
         renderItem={({ item }) => (
-          <ReportCard {...item} navigation={navigation}  />
+          <ReportCard {...item} navigation={navigation} />
         )}
         keyExtractor={(item) => item.id}
         style={styles.continer_table}
@@ -23,18 +22,18 @@ export default function MyReports({navigation}) {
   );
 }
 const styles = StyleSheet.create({
-  continer:{
-    marginVertical:50
+  continer: {
+    marginVertical: 50
   },
-  title:{textAlign: "center"},
-  continer_title:{
-    fontSize: 36 
+  title: { textAlign: "center" },
+  continer_title: {
+    fontSize: 36
   },
-  secondary_title:{
-    fontSize: 25 
+  secondary_title: {
+    fontSize: 25
   },
-  continer_table:{
-    marginVertical:20,
-    marginHorizontal:2
+  continer_table: {
+    marginVertical: 20,
+    marginHorizontal: 2
   }
 })
