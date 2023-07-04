987d556f425c9d0a466b4935dedf8ac8dbd19986
edend85 <117260021+edend85@users.noreply.github.com>
2023-07-04 14:42:10 +0300

    Update APIContext.jsx

--- src/Context/APIContext.jsx
+++ src/Context/APIContext.jsx
@@ -4,8 +4,9 @@ export const APIContext = createContext();
 
 export default function APIContextProvider({ children }) {
     const InsertReport = async (doc, email) => {
-        console.log('hi InsertReport :>> ', doc, email);
+        console.log('hi InsertReport :>> ', doc.date, email);
         try {
+            console.log('InsertReport :>> ');
             if (doc.date == null) return;
             const url = `${base_URL}/reports/AddReport`;
             const response = await fetch(url, {
@@ -15,8 +16,10 @@ export default function APIContextProvider({ children }) {
                 },
                 body: JSON.stringify({ report: doc, email: email }),
             });
-            if (response.ok) { }
-            else { }
+            if (response.ok) {
+                alert("דיווח נשלח בהצלחה!");
+            }
+            else { alert("אירעה שגיאת בעת שמירת דיווח") }
         } catch (err) {
             console.log("err :>> ", err);
         }
