3fd56534438358ac5972a918d4fa72f33f04d2c0
edend85 <117260021+edend85@users.noreply.github.com>
2023-07-22 15:39:06 +0300

    Update APIContext.jsx

--- src/Context/APIContext.jsx
+++ src/Context/APIContext.jsx
@@ -4,10 +4,57 @@ export const APIContext = createContext();
 
 export default function APIContextProvider({ children }) {
     const [report, setReport] = useState({});
+    const [currentUser, setCurrentUser] = useState({});
+    const [allReports, setAllReports] = useState([]);
+    const [visible, setVisible] = useState(false);
+
+    //Add Client to clients Array
+    const InsertNewUser = async (user) => {
+        try {
+            console.log('insert :>> ');
+            const url = `${base_URL}/users/Register`;
+            const response = await fetch(url, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(user),
+            });
+            if (!response.ok) {
+                throw new Error("Request failed with status " + response.status());
+            } else {
+                setVisible(true);
+            }
+        } catch (err) {
+            console.log("err :>> ", err);
+        }
+    };
+
+    // chack if the user is exsist
+    const ConfirmClient = async (e, p) => {
+        try {
+            let url = `${base_URL}/users/Login`;
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ email: e, password: p }),
+            });
+            if (response.ok) {
+                let user = await response.json();
+                setCurrentUser(user);
+            } else {
+                let error = await response.text();
+                throw new Error(error);
+            }
+        } catch (err) {
+            alert("שם משתמש או סיסמא לא תקינים");
+        }
+    };
     const InsertReport = async (doc, email) => {
         console.log('hi InsertReport :>> ', doc, email);
         try {
-            console.log('InsertReport :>> ', doc.date);
             if (doc.date == null) { return; }
             const url = `${base_URL}/reports/AddReport`;
             const response = await fetch(url, {
@@ -23,14 +70,32 @@ export default function APIContextProvider({ children }) {
                     place: doc.place,
                     details: doc.details,
                     image: doc.image,
-                    reporter: doc.reporter == "Anonymous" ? "Anonymous": doc.reporter,
                     email: email
                 }),
             });
             if (response.ok) {
                 alert("דיווח נשלח בהצלחה!");
             }
-            else { alert("אירעה שגיאת בעת שמירת דיווח") }
+        } catch (err) {
+            console.log("err :>> ", err);
+        }
+        setReport({})
+    };
+
+    const ShowMyReports = async (email) => {
+        try {
+            console.log('ShowMyReports :>> ', email);
+            const url = `${base_URL}/reports/ShowMyReports`;
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ email: email }),
+            });
+            if (response.ok) {
+                setAllReports(await response.json());
+            }
         } catch (err) {
             console.log("err :>> ", err);
         }
@@ -38,8 +103,17 @@ export default function APIContextProvider({ children }) {
 
     const value = {
         InsertReport,
+        setReport,
+        setCurrentUser,
+        ConfirmClient,
+        ShowMyReports,
+        InsertNewUser,
+        setVisible,
+        visible,
+        allReports,
+        currentUser,
         report,
-        setReport
+
     }
     return (
         <APIContext.Provider value={value}>
