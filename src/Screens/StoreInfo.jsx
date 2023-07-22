bb302935cee2ced5ac2c8d687de151cb6e844bfa
edend85 <117260021+edend85@users.noreply.github.com>
2023-07-22 18:09:24 +0300

    Update StoreInfo.jsx

--- src/Screens/StoreInfo.jsx
+++ src/Screens/StoreInfo.jsx
@@ -1,22 +1,22 @@
-import { View, Text, SafeAreaView,ScrollView } from "react-native";
+import { View, Text, SafeAreaView, ScrollView } from "react-native";
 import React, { useContext } from "react";
 import Accordion from "../Components/Accordion";
-import { SmokeyeContext } from "../Context/SmokEyeContext";
+import { APIContext } from "../Context/APIContext";
 
 
 export default function StoreInfo() {
-  const {infoData} = useContext(SmokeyeContext);
+  const { infoData } = useContext(APIContext);
   const businessData = infoData[0].business;
-  
+
   return (
     <SafeAreaView>
       <ScrollView>
-      <Text style={{ textAlign: "center", fontSize: 40 }}>
-        מידע לעסק
-      </Text>
-      {businessData.map((item, index) => (
-        <Accordion key={index} title={item.title} content={item.content} />
-      ))}
+        <Text style={{ textAlign: "center", fontSize: 40 }}>
+          מידע לעסק
+        </Text>
+        {businessData.map((item, index) => (
+          <Accordion key={index} title={item.title} content={item.content} />
+        ))}
       </ScrollView>
     </SafeAreaView>
   );
