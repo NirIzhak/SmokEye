e0d8ad1562dd6f803b4a220216afeb605fe027c3
edend85 <117260021+edend85@users.noreply.github.com>
2023-07-22 15:39:16 +0300

    Update MyReports.jsx

--- src/Screens/MyReports.jsx
+++ src/Screens/MyReports.jsx
@@ -1,44 +1,48 @@
-import { View, Text, FlatList,StyleSheet } from "react-native";
-import React, { useContext } from "react";
+import { View, Text, FlatList, StyleSheet } from "react-native";
+import { useContext, useEffect } from "react";
 import { SmokeyeContext } from "../Context/SmokEyeContext";
 import ReportCard from "../Components/ReportCard";
+import { APIContext } from "../Context/APIContext";
 
-export default function MyReports({navigation}) {
-  const { currentUser, allReports } = useContext(SmokeyeContext);
-  const reports = allReports.filter((r)=> r.reporter == `${currentUser.firstName} ${currentUser.lastName}`)
+export default function MyReports({ navigation }) {
+  const { allReports, ShowMyReports, currentUser } = useContext(APIContext);
 
+  useEffect(() => {
+    if (!allReports) return;
+    ShowMyReports(currentUser.email);
+  }, [allReports]);
 
   return (
     <View style={styles.continer} >
-      <Text style={[styles.continer_title,styles.title]}>
+      <Text style={[styles.continer_title, styles.title]}>
         הדיווחים שלי
       </Text>
-      <Text style={[styles.secondary_title,styles.title]}>סה"כ - 
-      {reports.length}</Text>
-       <FlatList
-        data={reports}
+      <Text style={[styles.secondary_title, styles.title]}>סה"כ -
+        {allReports.length}</Text>
+      <FlatList
+        data={allReports}
         renderItem={({ item }) => (
-          <ReportCard {...item} navigation={navigation}  />
+          <ReportCard {...item} navigation={navigation} />
         )}
         keyExtractor={(item) => item.id}
         style={styles.continer_table}
-      /> 
+      />
     </View>
   );
 }
 const styles = StyleSheet.create({
-  continer:{
-    marginVertical:50
+  continer: {
+    marginVertical: 50
   },
-  title:{textAlign: "center"},
-  continer_title:{
-    fontSize: 36 
+  title: { textAlign: "center" },
+  continer_title: {
+    fontSize: 36
   },
-  secondary_title:{
-    fontSize: 25 
+  secondary_title: {
+    fontSize: 25
   },
-  continer_table:{
-    marginVertical:20,
-    marginHorizontal:2
+  continer_table: {
+    marginVertical: 20,
+    marginHorizontal: 2
   }
 })
