a0b21c8798f56b3daaf00dc84e5df4577ce542cd
edend85 <117260021+edend85@users.noreply.github.com>
2023-07-22 18:09:20 +0300

    Update AllStyels.jsx

--- src/style/AllStyels.jsx
+++ src/style/AllStyels.jsx
@@ -1,16 +1,23 @@
-
-export const Colors={
-  primary:'#F39508',
+import { DefaultTheme } from "react-native-paper";
+export const Colors = {
+  primary: '#F39508',
   black: '#000',
   white: '#fff',
-  borderColor:"#8C8A89",
-  transparent:"transparent"
+  borderColor: "#8C8A89",
+  transparent: "transparent"
 }
-export const fontSizes={
-  XL:22,
+export const fontSizes = {
+  XL: 22,
   L: 20,
   M: 18,
   S: 16
 }
-
-  
+export const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "#F39508",
+    accent: "#f1c40f",
+  },
+};
