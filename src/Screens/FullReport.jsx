81f066f350430735abe35689a0e00ac6dc8a9902
edend85 <117260021+edend85@users.noreply.github.com>
2023-07-22 18:43:45 +0300

    Update FullReport.jsx

--- src/Screens/FullReport.jsx
+++ src/Screens/FullReport.jsx
@@ -1,13 +1,15 @@
 import { View, Text, SafeAreaView, Image, StyleSheet } from "react-native";
 import React from "react";
 
-export default function FullReport({ route }) {
-  const { id, details, image, address, date, type } = route.params;
+export default function FullReport({ date, type, address, place, details, image }) {
+
   return (
     <SafeAreaView style={styles.continer}>
       <Text style={styles.title}>פרטי דיווח מלאים:</Text>
-      <Text style={styles.text}>תיאור: {details}</Text>
-      <Text style={styles.text}>כתובת מלאה: {address[0].street + " " + address[0].streetNum + ", " + address[0].city}</Text>
+      <Text style={styles.text}>עסק / פרטי: {type}</Text>
+      <Text style={styles.text}>תיאור: xxxx {details}</Text>
+      <Text style={styles.text}>כתובת מלאה: {address}</Text>
+      <Text style={styles.text}>מיקום : {place}</Text>
       <Text style={styles.text}>תאריך: {date}</Text>
       <Text style={styles.imageText}>תמונה:</Text>
       <Image source={{ uri: image }} style={styles.image} />
@@ -16,10 +18,10 @@ export default function FullReport({ route }) {
 }
 
 const styles = StyleSheet.create({
-  continer:{
-      flex:1,
-      justifyContent:'center',
-      backgroundColor:'#fff'
+  continer: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
   image: {
     width: "80%",
@@ -28,18 +30,18 @@ const styles = StyleSheet.create({
     marginRight: "auto",
     marginLeft: "auto",
   },
-  text : {
+  text: {
     textAlign: 'right',
     fontSize: 20,
     margin: 10,
     marginRight: 25
   },
-  title:{
+  title: {
     textAlign: 'center',
     fontSize: 35,
     marginTop: 10
   },
-  imageText :{
+  imageText: {
     textAlign: 'center',
     fontSize: 20,
     margin: 10,
