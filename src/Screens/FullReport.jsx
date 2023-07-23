3e6214576656f6daa70da263a62686fa5337a966
edend85 <117260021+edend85@users.noreply.github.com>
2023-07-23 15:35:10 +0300

    Update FullReport.jsx

--- src/Screens/FullReport.jsx
+++ src/Screens/FullReport.jsx
@@ -1,18 +1,22 @@
 import { View, Text, SafeAreaView, Image, StyleSheet } from "react-native";
-import React from "react";
+import React, { useContext } from "react";
+import { SmokeyeContext } from "../Context/SmokEyeContext";
 
-export default function FullReport({ date, type, address, place, details, image }) {
-
+export default function FullReport() {
+  const { report } = useContext(SmokeyeContext);
+  let a = report.address[0].street + " " + report.address[0].streetNum + " " + report.address[0].city;
+  const dateArr = report.date.split(" ");
+  console.log('report :>> ', report);
   return (
     <SafeAreaView style={styles.continer}>
       <Text style={styles.title}>פרטי דיווח מלאים:</Text>
-      <Text style={styles.text}>עסק / פרטי: {type}</Text>
-      <Text style={styles.text}>תיאור: {details}</Text>
-      <Text style={styles.text}>כתובת מלאה: {address}</Text>
-      <Text style={styles.text}>מיקום : {place}</Text>
-      <Text style={styles.text}>תאריך: {date}</Text>
+      <Text style={styles.text}>עסק / פרטי: {report.type}</Text>
+      <Text style={styles.text}>תיאור: {report.details}</Text>
+      <Text style={styles.text}>כתובת מלאה: {a}</Text>
+      <Text style={styles.text}>מיקום : {report.place}</Text>
+      <Text style={styles.text}>תאריך: {dateArr[0]}</Text>
       <Text style={styles.imageText}>תמונה:</Text>
-      <Image source={{ uri: image }} style={styles.image} />
+      <Image source={{ uri: report.image }} style={styles.image} />
     </SafeAreaView>
   );
 }
