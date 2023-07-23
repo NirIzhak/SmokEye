0bc492a9a7a2614bee881a89cffc21702393153a
edend85 <117260021+edend85@users.noreply.github.com>
2023-07-23 14:06:16 +0300

    Update FullReport.jsx

--- src/Screens/FullReport.jsx
+++ src/Screens/FullReport.jsx
@@ -7,7 +7,7 @@ export default function FullReport({ date, type, address, place, details, image 
     <SafeAreaView style={styles.continer}>
       <Text style={styles.title}>פרטי דיווח מלאים:</Text>
       <Text style={styles.text}>עסק / פרטי: {type}</Text>
-      <Text style={styles.text}>תיאור: xxxx {details}</Text>
+      <Text style={styles.text}>תיאור: {details}</Text>
       <Text style={styles.text}>כתובת מלאה: {address}</Text>
       <Text style={styles.text}>מיקום : {place}</Text>
       <Text style={styles.text}>תאריך: {date}</Text>
