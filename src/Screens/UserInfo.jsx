792728c15b607d02aa5dc54f853b48ecf0785902
edend85 <117260021+edend85@users.noreply.github.com>
2023-08-06 15:59:39 +0300

    Update UserInfo.jsx

--- src/Screens/UserInfo.jsx
+++ src/Screens/UserInfo.jsx
@@ -1,16 +1,17 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, StyleSheet } from "react-native";
 import React, { useContext } from "react";
 import Accordion from "../Components/Accordion";
 import { APIContext } from "../Context/APIContext";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 export default function UserInfo() {
   const { infoData } = useContext(APIContext);
   const personData = infoData[0].person;
 
   return (
-    <SafeAreaView>
-      <Text style={{ textAlign: "center", fontSize: 40 }}>
-        מידע למשתמש
+    <SafeAreaView style={styles.continer}>
+      <Text style={styles.title}>
+        תקנות במרחב הציבורי
       </Text>
       {personData.map((item, index) => (
         <Accordion key={index} title={item.title} content={item.content} />
@@ -18,3 +19,18 @@ export default function UserInfo() {
     </SafeAreaView>
   );
 }
+const styles = StyleSheet.create({
+  continer: {
+    flex: 1,
+    paddingVertical: 100,
+    height: "100%",
+    backgroundColor: Colors.white
+  },
+  title: {
+    textAlign: "center",
+    fontSize: 36,
+    marginBottom: 10
+  }
+})
+
+
