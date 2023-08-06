65550c47d061df30d3979c1df4833300da93a992
Nir Izhak <nir676676@gmail.com>
2023-08-06 16:42:00 +0300

    Add image required

--- src/Screens/NewReport.jsx
+++ src/Screens/NewReport.jsx
@@ -1,4 +1,17 @@
-import { View, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, Keyboard, Image, Alert, StyleSheet, Button, ScrollView, Modal } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Image,
+  Alert,
+  StyleSheet,
+  Button,
+  ScrollView,
+  Modal,
+} from "react-native";
 import { Dropdown } from "react-native-element-dropdown";
 import React, { useEffect, useState, useContext } from "react";
 import * as Location from "expo-location";
@@ -10,39 +23,68 @@ import { theme, Colors, fontSizes } from "../style/AllStyels";
 import { Popstyles } from "../style/PopUpModal";
 
 export default function NewReport() {
-
-  const { data, imageUri, setImageUri, des, setDes } = useContext(SmokeyeContext);
-  const { popMsgReport, setpopMsgReport, city, setCity, street, setStreet, streetNum, SetStreetNum, ImageUploader, setReport, report, InsertReport, currentUser, latitude, longitude, GetAddress, GetLocationByAddress } = useContext(APIContext);
+  const { data, imageUri, setImageUri, des, setDes } =
+    useContext(SmokeyeContext);
+  const {
+    popMsgReport,
+    setpopMsgReport,
+    city,
+    setCity,
+    street,
+    setStreet,
+    streetNum,
+    SetStreetNum,
+    ImageUploader,
+    setReport,
+    report,
+    InsertReport,
+    currentUser,
+    latitude,
+    longitude,
+    GetAddress,
+    GetLocationByAddress,
+  } = useContext(APIContext);
   const date = new Date();
 
   const [checked, setChecked] = useState("Business");
   const [value, setValue] = useState(null);
   const [BusName, setBusName] = useState("");
   const [select, setSelect] = useState(false);
+  const [isImageRequired, setIsImageRequired] = useState(false);
 
   const handlePress = () => {
     Keyboard.dismiss();
   };
   const hidePopupModal = () => {
     setpopMsgReport(false);
-  }
+  };
   //creating ner report
   const createReport = async () => {
-    //splash window
-    const locationFromAddress = await GetLocationByAddress(street, streetNum, city);
-    const imageLink = await ImageUploader(imageUri);
-    await setReport({
-      date: `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`,
-      type: `${checked}`,
-      location: [
-        `${locationFromAddress.lat || latitude}`,
-        `${locationFromAddress.lon || longitude}`,
-      ],
-      address: [{ street: `${street}`, streetNum: `${streetNum}`, city: `${city}` }],
-      place: checked === "Business" ? `${BusName}` : `${value}`,
-      details: `${des}`,
-      image: `${imageLink}`,
-    });
+    if (!imageUri) {
+      setIsImageRequired(true);
+    } else {
+      //splash window
+      const locationFromAddress = await GetLocationByAddress(
+        street,
+        streetNum,
+        city
+      );
+      const imageLink = await ImageUploader(imageUri);
+      await setReport({
+        date: `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`,
+        type: `${checked}`,
+        location: [
+          `${locationFromAddress.lat || latitude}`,
+          `${locationFromAddress.lon || longitude}`,
+        ],
+        address: [
+          { street: `${street}`, streetNum: `${streetNum}`, city: `${city}` },
+        ],
+        place: checked === "Business" ? `${BusName}` : `${value}`,
+        details: `${des}`,
+        image: `${imageLink}`,
+      });
+    }
   };
   //check new report
   useEffect(() => {
@@ -50,7 +92,7 @@ export default function NewReport() {
     InsertReport(report, currentUser.email);
   }, [report]);
 
-  //Camera function 
+  //Camera function
   //taking picture from camera
   const openCamera = async () => {
     let permissionResult = await ImagePicker.requestCameraPermissionsAsync();
@@ -60,7 +102,10 @@ export default function NewReport() {
       return;
     }
 
-    let pickerResult = await ImagePicker.launchCameraAsync({ base64: true, quality: 1.0 });
+    let pickerResult = await ImagePicker.launchCameraAsync({
+      base64: true,
+      quality: 1.0,
+    });
 
     if (!pickerResult.canceled) {
       await setImageUri(pickerResult.assets[0].base64);
@@ -76,7 +121,10 @@ export default function NewReport() {
       return;
     }
 
-    let pickerResult = await ImagePicker.launchImageLibraryAsync({ base64: true, quality: 1.0 });
+    let pickerResult = await ImagePicker.launchImageLibraryAsync({
+      base64: true,
+      quality: 1.0,
+    });
 
     if (!pickerResult.canceled) {
       setImageUri(pickerResult.assets[0].base64);
@@ -126,8 +174,7 @@ export default function NewReport() {
         <TextInput
           style={styles.Private_input}
           onChangeText={(text) => setBusName(text)}
-        >
-        </TextInput>
+        ></TextInput>
       </>
     );
   };
@@ -144,7 +191,10 @@ export default function NewReport() {
       <TouchableWithoutFeedback onPress={handlePress}>
         <PaperProvider theme={theme}>
           <View style={styles.container}>
-            <Text style={[styles.title, { fontSize: fontSizes.XL }]}> על מה הדיווח?</Text>
+            <Text style={[styles.title, { fontSize: fontSizes.XL }]}>
+              {" "}
+              על מה הדיווח?
+            </Text>
             <View style={styles.radio_btn}>
               <View style={styles.radioButtonContainer}>
                 <RadioButton.Android
@@ -186,8 +236,11 @@ export default function NewReport() {
                 <Text style={[styles.btn]}>העלאת תמונה</Text>
               )}
             </TouchableOpacity>
-            {imageUri && (
-              <Text>נבחרה תמונה!</Text>
+            {imageUri && <Text>נבחרה תמונה!</Text>}
+            {isImageRequired && !imageUri && (
+              <Text style={{ color: "red", marginBottom: 10 }}>
+                חובה להעלות תמונה כדי לדווח
+              </Text>
             )}
             <Text style={styles.title}>פרטי מיקום:</Text>
             <View style={styles.addressContainer}>
@@ -218,8 +271,9 @@ export default function NewReport() {
                 >
                   <Text style={styles.btn}>מצא אותי !</Text>
                 </TouchableOpacity>
-              ) : <Text>זיהה את מיקומך ? ניתן להכניס ידנית </Text>
-              }
+              ) : (
+                <Text>זיהה את מיקומך ? ניתן להכניס ידנית </Text>
+              )}
             </View>
             <View style={styles.sendReport}>
               <Button
@@ -228,43 +282,45 @@ export default function NewReport() {
                 onPress={() => {
                   createReport();
                 }}
-              >
-
-              </Button>
+              ></Button>
             </View>
           </View>
         </PaperProvider>
       </TouchableWithoutFeedback>
-      {
-        popMsgReport ?
-          <>
-            <View>
-              <Modal
-                visible={popMsgReport}
-                animationType="fade"
-                transparent={true}
-                onRequestClose={hidePopupModal}
-              >
-                <View style={Popstyles.modalContainer}>
-                  <View style={Popstyles.modalContent}>
-                    <Text style={Popstyles.messageText}>דיווח נשלח בהצלחה ! </Text>
-                    <View style={{ alignItems: 'center' }}>
-                      <Image source={{ uri: "https://cdn-icons-png.flaticon.com/512/1102/1102355.png?w=740&t=st=1690886025~exp=1690886625~hmac=5516a06b0266fe418d8604dcc0fc5935f96153877b94db73796af0874f383cd5" }} style={{
+      {popMsgReport ? (
+        <>
+          <View>
+            <Modal
+              visible={popMsgReport}
+              animationType="fade"
+              transparent={true}
+              onRequestClose={hidePopupModal}
+            >
+              <View style={Popstyles.modalContainer}>
+                <View style={Popstyles.modalContent}>
+                  <Text style={Popstyles.messageText}>
+                    דיווח נשלח בהצלחה !{" "}
+                  </Text>
+                  <View style={{ alignItems: "center" }}>
+                    <Image
+                      source={{
+                        uri: "https://cdn-icons-png.flaticon.com/512/1102/1102355.png?w=740&t=st=1690886025~exp=1690886625~hmac=5516a06b0266fe418d8604dcc0fc5935f96153877b94db73796af0874f383cd5",
+                      }}
+                      style={{
                         height: 180,
                         width: 180,
-                      }}></Image>
-                    </View>
-                    <TouchableOpacity onPress={hidePopupModal}>
-                      <Text style={Popstyles.closeButton}>סגור</Text>
-                    </TouchableOpacity>
+                      }}
+                    ></Image>
                   </View>
+                  <TouchableOpacity onPress={hidePopupModal}>
+                    <Text style={Popstyles.closeButton}>סגור</Text>
+                  </TouchableOpacity>
                 </View>
-              </Modal>
-            </View>
-          </>
-          :
-          null
-      }
+              </View>
+            </Modal>
+          </View>
+        </>
+      ) : null}
     </>
   );
 }
@@ -274,13 +330,13 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: Colors.white,
-    height: "100%"
+    height: "100%",
   },
   title: {
     marginBottom: 10,
     marginTop: 50,
     textAlign: "center",
-    fontWeight: "bold"
+    fontWeight: "bold",
   },
   radioButtonContainer: {
     flexDirection: "row",
@@ -299,14 +355,14 @@ const styles = StyleSheet.create({
     borderBottomColor: Colors.black,
     borderBottomWidth: 1,
     marginTop: 15,
-    textAlign: 'right'
+    textAlign: "right",
   },
   input_Text: {
     borderColor: Colors.borderColor,
     borderWidth: 1,
     borderRadius: 5,
     padding: 5,
-    direction: "rtl"
+    direction: "rtl",
   },
   report_Details: {
     width: "85%",
@@ -358,7 +414,7 @@ const styles = StyleSheet.create({
     textAlignVertical: "center",
   },
   dropdown: {
-    textAlign: 'right',
+    textAlign: "right",
     height: 50,
     backgroundColor: Colors.white,
     borderRadius: 5,
