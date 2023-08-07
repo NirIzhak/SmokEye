438656d27a5653375abea1c58670540a19155a16
edend85 <117260021+edend85@users.noreply.github.com>
2023-08-07 22:07:42 +0300

    Update Register.jsx

--- src/Screens/Register.jsx
+++ src/Screens/Register.jsx
@@ -1,4 +1,4 @@
-import { Text, View, StyleSheet, Switch, TouchableOpacity, KeyboardAvoidingView, SafeAreaView, Image, Modal, Alert } from "react-native";
+import { Text, View, StyleSheet, Switch, TouchableOpacity, KeyboardAvoidingView, SafeAreaView, Image, Modal, Alert, ScrollView } from "react-native";
 import { useContext, useState, useEffect } from "react";
 import { SmokeyeContext } from "../Context/SmokEyeContext";
 import { Colors, fontSizes } from "../style/AllStyels";
@@ -81,124 +81,124 @@ export default function Register({ navigation }) {
   };
   return (
     <>
-      <SafeAreaView>
+      <ScrollView>
+        <KeyboardAvoidingView onPress={handlePress}>
+          <View style={styles.container}>
+            <View>
+              <Text style={styles.h1_title}>הרשמה</Text>
+            </View>
+            <View style={styles.rowInputConteiner}>
+              <TextInput
+                activeOutlineColor={Colors.primary}
+                style={styles.input2}
+                mode="outlined"
+                label="שם פרטי"
+                onChangeText={(text) => setFirstName(text)}
+              />
+              <TextInput
+                activeOutlineColor={Colors.primary}
+                mode="outlined"
+                style={styles.input2}
+                label="שם משפחה"
+                onChangeText={(text) => setlastName(text)}
+              />
+            </View>
+            <TextInput
+              activeOutlineColor={Colors.primary}
+              mode="outlined"
+              style={styles.input}
+              label="Email"
+              onChangeText={(text) => setEmail(text)}
+              keyboardType="email-address"
+            />
+            <TextInput
+              activeOutlineColor={Colors.primary}
+              mode="outlined"
+              style={styles.input}
+              label="סיסמא"
+              onChangeText={(text) => setPassword(text)}
+            />
+            <TextInput
+              activeOutlineColor={Colors.primary}
+              mode="outlined"
+              style={styles.input}
+              label="פלאפון"
+              onChangeText={(text) => setPhone(text)}
+              keyboardType="phone-pad"
+            />
+            <TextInput
+              activeOutlineColor={Colors.primary}
+              mode="outlined"
+              style={styles.input}
+              label="כתובת"
+              onChangeText={(text) => setAddress(text)}
+            />
+            <TouchableOpacity style={styles.button2} onPress={createTwoButtonAlert}>
+              {photo ? (
+                <Text style={[styles.title, { fontSize: fontSizes.M }]}>התמונה עלתה!</Text>
+              ) : (
+                <Text style={[styles.title, { fontSize: fontSizes.S }]}>העלאת תמונה</Text>
+              )}
+            </TouchableOpacity>
 
-      </SafeAreaView>
-      <View style={styles.container}>
-        <View>
-          <Text style={styles.h1_title}>הרשמה</Text>
-        </View>
-        <View style={styles.rowInputConteiner}>
-          <TextInput
-            activeOutlineColor={Colors.primary}
-            style={styles.input2}
-            mode="outlined"
-            label="שם פרטי"
-            onChangeText={(text) => setFirstName(text)}
-          />
-          <TextInput
-            activeOutlineColor={Colors.primary}
-            mode="outlined"
-            style={styles.input2}
-            label="שם משפחה"
-            onChangeText={(text) => setlastName(text)}
-          />
-        </View>
-        <TextInput
-          activeOutlineColor={Colors.primary}
-          mode="outlined"
-          style={styles.input}
-          label="Email"
-          onChangeText={(text) => setEmail(text)}
-          keyboardType="email-address"
-        />
-        <TextInput
-          activeOutlineColor={Colors.primary}
-          mode="outlined"
-          style={styles.input}
-          label="סיסמא"
-          onChangeText={(text) => setPassword(text)}
-        />
-        <TextInput
-          activeOutlineColor={Colors.primary}
-          mode="outlined"
-          style={styles.input}
-          label="פלאפון"
-          onChangeText={(text) => setPhone(text)}
-          keyboardType="phone-pad"
-        />
-        <TextInput
-          activeOutlineColor={Colors.primary}
-          mode="outlined"
-          style={styles.input}
-          label="כתובת"
-          onChangeText={(text) => setAddress(text)}
-        />
-        <TouchableOpacity style={styles.button2} onPress={createTwoButtonAlert}>
-          {photo ? (
-            <Text style={[styles.title, { fontSize: fontSizes.M }]}>התמונה עלתה!</Text>
-          ) : (
-            <Text style={[styles.title, { fontSize: fontSizes.S }]}>העלאת תמונה</Text>
-          )}
-        </TouchableOpacity>
-
-        <View style={styles.smoke_comtiner}>
-          <Text style={styles.title}>לא מעשן</Text>
-          <Switch
-            trackColor={{ false: "#767577", true: "#7CC69E" }}
-            thumbColor={smoke ? "#5CEE9F" : "#f4f3f4"}
-            ios_backgroundColor="#3e3e3e"
-            onValueChange={toggleSwitch}
-            value={smoke}
-            style={{ margin: 10 }}
-          />
-          <Text style={styles.title}>מעשן</Text>
-        </View>
-        <TouchableOpacity onPress={AddClient} style={styles.button}>
-          <Text style={styles.title}>הרשם</Text>
-        </TouchableOpacity>
-        {
-          visible ?
-            <>
-              <View>
-                <Modal
-                  visible={visible}
-                  animationType="fade"
-                  transparent={true}
-                  onRequestClose={hidePopupModal}
-                >
-                  <View style={Popstyles.modalContainer}>
-                    <View style={Popstyles.modalContent}>
-                      <Text style={Popstyles.messageText}> ההרשמה התבצעה בהצלחה !</Text>
-                      <View style={{ alignItems: 'center' }}>
-                        <Image source={{ uri: "https://cdn-icons-png.flaticon.com/512/1102/1102355.png?w=740&t=st=1690886025~exp=1690886625~hmac=5516a06b0266fe418d8604dcc0fc5935f96153877b94db73796af0874f383cd5" }} style={{
-                          height: 180,
-                          width: 180,
-                        }}></Image>
+            <View style={styles.smoke_comtiner}>
+              <Text style={styles.title}>לא מעשן</Text>
+              <Switch
+                trackColor={{ false: "#767577", true: "#7CC69E" }}
+                thumbColor={smoke ? "#5CEE9F" : "#f4f3f4"}
+                ios_backgroundColor="#3e3e3e"
+                onValueChange={toggleSwitch}
+                value={smoke}
+                style={{ margin: 10 }}
+              />
+              <Text style={styles.title}>מעשן</Text>
+            </View>
+            <TouchableOpacity onPress={AddClient} style={styles.button}>
+              <Text style={styles.title}>הרשם</Text>
+            </TouchableOpacity>
+            {
+              visible ?
+                <>
+                  <View>
+                    <Modal
+                      visible={visible}
+                      animationType="fade"
+                      transparent={true}
+                      onRequestClose={hidePopupModal}
+                    >
+                      <View style={Popstyles.modalContainer}>
+                        <View style={Popstyles.modalContent}>
+                          <Text style={Popstyles.messageText}> ההרשמה התבצעה בהצלחה !</Text>
+                          <View style={{ alignItems: 'center' }}>
+                            <Image source={{ uri: "https://cdn-icons-png.flaticon.com/512/1102/1102355.png?w=740&t=st=1690886025~exp=1690886625~hmac=5516a06b0266fe418d8604dcc0fc5935f96153877b94db73796af0874f383cd5" }} style={{
+                              height: 180,
+                              width: 180,
+                            }}></Image>
+                          </View>
+                          <TouchableOpacity onPress={hidePopupModal}>
+                            <Text style={Popstyles.closeButton}>סגור</Text>
+                          </TouchableOpacity>
+                        </View>
                       </View>
-                      <TouchableOpacity onPress={hidePopupModal}>
-                        <Text style={Popstyles.closeButton}>סגור</Text>
-                      </TouchableOpacity>
-                    </View>
+                    </Modal>
                   </View>
-                </Modal>
-              </View>
-            </>
-            :
-            null
-        }
-      </View>
+                </>
+                :
+                null
+            }
+          </View>
+        </KeyboardAvoidingView>
+      </ScrollView>
     </>
 
   );
 }
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
     height: "100%",
-    backgroundColor: Colors.white
+    backgroundColor: Colors.white,
+    paddingVertical: 80,
   },
   title: {
     textAlign: "center",
@@ -207,7 +207,7 @@ const styles = StyleSheet.create({
   h1_title: {
     textAlign: "center",
     fontSize: 40,
-    marginBottom: 30,
+    marginBottom: 20,
   },
   rowInputConteiner: {
     marginBottom: 12,
@@ -223,8 +223,10 @@ const styles = StyleSheet.create({
   },
   input2: {
     backgroundColor: Colors.white,
-    margin: 15,
-    width: "45%",
+    marginBottom: 10,
+    marginEnd: 2,
+    marginStart: 2,
+    width: "50%",
 
   },
   button: {
@@ -236,8 +238,11 @@ const styles = StyleSheet.create({
   },
   button2: {
     backgroundColor: Colors.primary,
-    width: "30%",
+    width: "50%",
     padding: 15,
+    borderWidth: 1,
+    backgroundColor: Colors.white,
+    borderColor: Colors.primary,
     borderRadius: 5,
     marginTop: 20,
   },
